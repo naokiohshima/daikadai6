7cac939aa14aef50bb3efc2e62f7c376fb43a25b
naoki ohshima <saylako@icloud.com>
2018-02-27 10:45:10 +0000

    heroku

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -21,6 +21,34 @@ class CommentsController < ApplicationController
     end
   end
 
+  def edit
+   @comment = Comment.find(params[:id])
+   @event = @comment.event
+  end
+
+  def update
+    @comment = Comment.find(params[:id])
+    @blog = @comment.event
+    if @comment.update(comment_params)
+      redirect_to events_path, notice: "コメントを更新しました！"
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    respond_to do |format|
+      if @comment.destroy
+        format.html { redirect_to blog_path(@comment), notice: 'コメントを削除しました。' }
+        format.js { render :index }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+
   private
     def comment_params
       params.require(:comment).permit(:event_id, :content)
